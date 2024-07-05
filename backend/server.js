07d7d5bd1a64d777287438f3775655e5fd3d65cb
Parag <parag0157@gmail.com>
2024-07-06 00:19:37 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const moodsRouter = require("./routes/mood.js");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/moods",moodsRouter);
+
+app.get("/",(req,res)=>{
+    res.send("Hello World!");
+});
+
+app.listen(3001,()=>{
+    console.log('Server is running on port 3001');
+});
